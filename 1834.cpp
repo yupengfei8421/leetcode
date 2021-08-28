0f0502744d787feb94195e621afd99923ef70915
yupengfei <1074064684@qq.com>
2021-08-28 22:07:33 +0800

    1834 submit

--- 1834.cpp
+++ 1834.cpp
@@ -12,39 +12,37 @@ private:
 
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
+        // 根据task进入队列的先后顺序，对其序号进行排序
         int n = tasks.size();
         vector<int> indices(n);
-        iota(indices.begin(), indices.end(), 0);
-        sort(indices.begin(), indices.end(), [&](int i, int j) {
-            return tasks[i][0] < tasks[j][0];
+        iota(indices.begin(),indices.end(),0);
+        sort(indices.begin(),indices.end(),[&](int i, int j){
+            return tasks[i][0]< tasks[j][0];
         });
-
-        vector<int> ans;
-        // 优先队列
-        priority_queue<PII, vector<PII>, greater<PII>> q;
-        // 时间戳
+        vector<int> answer;
+        //优先队列，存储处理tasks的先后次序
+        priority_queue<PII, vector<PII>, greater<PII>> q;//std::greater 的特化为任何指针类型产生严格全序
+        //时间戳，存储cpu的当前时间
         LL timestamp = 0;
-        // 数组上遍历的指针
-        int ptr = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            // 如果没有可以执行的任务，直接快进
-            if (q.empty()) {
-                timestamp = max(timestamp, (LL)tasks[indices[ptr]][0]);
+        //指针，用于遍历 
+        int ptr =0;
+        for(int i=0; i<n; ++i){
+            //如果没有可以执行的任务，直接快进
+            if (q.empty()){
+                timestamp = max(timestamp,(LL)tasks[indices[ptr]][0])
             }
-            // 将所有小于等于时间戳的任务放入优先队列
-            while (ptr < n && tasks[indices[ptr]][0] <= timestamp) {
-                q.emplace(tasks[indices[ptr]][1], indices[ptr]);
+            //将所有小于等于时间戳的任务放入优先队列中
+            while((ptr<n)&& tasks[indices[ptr]][0]<timestamp){
+                q.emplace(tasks[indices[ptr]][1],indices[ptr]);//有序放入优先队列中
                 ++ptr;
             }
-            // 选择处理时间最小的任务
-            auto&& [process, index] = q.top();
+            //处理时间最小的任务
+            auto && [process,index]=q.top();
             timestamp += process;
-            ans.push_back(index);
+            answer.push_back(index);
             q.pop();
-        }
-        
-        return ans;
+        }     
+        return answer;
     }
 };
  
