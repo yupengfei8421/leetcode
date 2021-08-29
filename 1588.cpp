6a1a00663c6b60648acf4ee00f7bb5b63d60dcff
yupengfei <1074064684@qq.com>
2021-08-29 19:38:44 +0800

    answer

--- 1588.cpp
+++ 1588.cpp
@@ -25,3 +25,24 @@ int main(){
     }
     cout<<"answer="<<solution.sumOddLengthSubarrays(test)<<endl;
 }
+
+// 关键就是，如何计算一个数字在多少个奇数长度的数组中出现过？
+
+// 对于一个数字，它所在的数组，可以在它前面再选择 0, 1, 2, ... 个数字，一共有 left = i + 1 个选择；
+
+// 可以在它后面再选择 0, 1, 2, ... 个数字，一共有 right = n - i 个选择。
+
+// 如果在前面选择了偶数个数字，那么在后面，也必须选择偶数个数字，这样加上它自身，才构成奇数长度的数组；
+
+// 如果在前面选择了奇数个数字，那么在后面，也必须选择奇数个数字，这样加上它自身，才构成奇数长度的数组；
+
+// 数字前面共有 left 个选择，其中偶数个数字的选择方案有 left_even = (left + 1) / 2 个，奇数个数字的选择方案有 left_odd = left / 2 个；
+
+// 数字后面共有 right 个选择，其中偶数个数字的选择方案有 right_even = (right + 1) / 2 个，奇数个数字的选择方案有 right_odd = right / 2 个；
+
+// 所以，每个数字一共在 left_even * right_even + left_odd * right_odd 个奇数长度的数组中出现过。
+
+// 作者：liuyubobobo
+// 链接：https://leetcode-cn.com/problems/sum-of-all-odd-length-subarrays/solution/cong-on3-dao-on-de-jie-fa-by-liuyubobobo/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
